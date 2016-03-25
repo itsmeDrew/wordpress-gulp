d5fa5ad4ecaf670a7635d9712a4080ce3b284f7f
Drew Roberts <drew@itsmedrew.com>
2016-03-25 16:58:34 -0400

    base block styling WIP

--- src/templates/blog-tpl.php
+++ src/templates/blog-tpl.php
@@ -11,6 +11,15 @@ get_header();
 
 <?php include(TEMPLATEPATH . '/subhead.php'); ?>
 
+<section class="hero">
+  <div class="hero__content container">
+    <h2 class="hero__heading">july 11-17, 2016</h2>
+    <p>
+      Want to take this year’s family vacation to a Whole ‘Notha Level?! Gear up for a week’s worth of Adventure—all leading up to our TWO Green Carpet events! Make this vacation an Adventure you’ll never forget—ONLY at It Works! Week!
+    </p>
+  </div>
+</section>
+
 <section class="blog container">
   <?php $queryBlog = new WP_Query(array( 'posts_per_page' => 5)); ?>
   <?php if ($queryBlog->have_posts()) : while ($queryBlog->have_posts()) : $queryBlog->the_post(); ?>
