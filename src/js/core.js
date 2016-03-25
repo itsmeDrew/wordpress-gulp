66e65fed9bff6a9e5a889aa61823f83a34e90940
Drew Roberts <drew@itsmedrew.com>
2016-03-25 14:14:16 -0400

    Comment styling

--- src/js/core.js
+++ src/js/core.js
@@ -1,4 +1,6 @@
-// ==== CORE ==== //
+//////////////////////
+// CORE
+//////////////////////
 
 // A simple wrapper for all your custom jQuery; everything in this file will be run on every page
 ;(function($){
--- src/js/page-loader.js
+++ src/js/page-loader.js
@@ -1,4 +1,6 @@
-// ==== WP AJAX PAGE LOADER ==== //
+//////////////////////
+// AJAX PAGE LOADER
+//////////////////////
 
 // Invoke the AJAX page loader; this is in its own file as the script is conditionally loaded by the theme
 ;(function($){
